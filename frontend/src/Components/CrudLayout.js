83fc14fb646a6f3dd1307da438a2eda9be6ed7a8
DarshVipani <darshvipani@gmail.com>
2024-10-10 12:16:33 +0530

    Update CrudLayout.js

--- frontend/src/Components/CrudLayout.js
+++ frontend/src/Components/CrudLayout.js
@@ -44,11 +44,7 @@ function CrudLayout() {
 
             </ul>
 
-            {/* <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-             */}
+            
              <div>
             <img style={{width:"120px",height:"70px",borderRadius:"5px"}} src={img} alt="Logo" className='me-3'/>
            </div>
